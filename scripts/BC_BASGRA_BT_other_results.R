32c5db8a9d07e12d4fe28e26f2bb9fbfab7460ce
Simon Woodward <simon.woodward@dairynz.co.nz>
2019-06-14 09:26:54 +1200

    Other results.

--- scripts/BC_BASGRA_BT_other_results.R
+++ scripts/BC_BASGRA_BT_other_results.R
@@ -1,18 +1,20 @@
 # other outputs
 
+library(scales)
+
 cat(file=stderr(), "Reading checkpoint after BASGRA results", "\n")
 load(file=paste(scenario, "/checkpoint_after_results.RData", sep=""))
 
 source("scripts/BC_export_parModes.R")
 # source("scripts/BC_plot_parameters_traceplots.R") # very slow! but useful for convergence check
-source("scripts/BC_plot_parameters_priorbeta_histograms.R")
+# source("scripts/BC_plot_parameters_priorbeta_histograms.R") # now using gpplot version
 # outputMax[which(outputNames=="CLV")] <- 200 # it's pretty awkward to set these programmatically
 # outputMax[which(outputNames=="CST")] <- 100
 # outputMax[which(outputNames=="TILTOT")] <- 10000
 # outputMax[which(outputNames=="WCL")] <- 60
 # source("scripts/BC_plot_outputs_data.R")
 
-# run with MAP parameters (can run shorter time window)
+# run with MAP parameters (can run shorter time window if desired to see detail detail)
 s <- 1
 for (s in 1:nSites){
   
@@ -21,11 +23,10 @@ for (s in 1:nSites){
   # dyn.load(BASGRA_DLL) # useful for rerunning this loop
   source(sitesettings_filenames[[s]]) # site initialisation
   # modify simulation length if desired
-  # year_start     <- as.integer(2011)
-  # doy_start      <- as.integer(244) # 1 September
-  # year_stop      <- as.integer(2017)
-  # year_stop      <- as.integer(2014)
-  # doy_stop       <- as.integer(151) # 31 May
+  year_start     <- as.integer(2011)
+  doy_start      <- as.integer(121) # 1 May
+  year_stop      <- as.integer(2014)
+  doy_stop       <- as.integer(120) # 30 April
   NDAYS_all <- NDAYS # will need to restore this for other functions
   NDAYS          <- as.integer(difftime(
     as.Date(doy_stop -1, origin=paste(year_stop ,1,1,sep="-")), 
@@ -42,48 +43,33 @@ for (s in 1:nSites){
   # create output lists
   leg <- vector("character", 1)
   list_output <- vector("list", 1)
-  leg <- vector("character", 5)
-  list_output <- vector("list", 5)
 
   # run MAP and scenarios
-  leg[1] <- "Current"
-  list_output[[1]] <- run_model(y = matrix(0, NDAYS, NOUT))
+  k <- 1
+  leg[k] <- "Current"
+  list_output[[k]] <- run_model(y = matrix(0, NDAYS, NOUT))
   
-  # leg[2] <- "Irrig+50mm"
-  # matrix_weather <- list_matrix_weather[[s]]
-  # ii <- matrix_weather[,2] %in% c(274,305,335,1,32,60,91,121) # irrig days
-  # matrix_weather[ii,6] <- matrix_weather[ii,6] + 50.0 # add irrig
-  # list_output[[2]] <- run_model()
-  # matrix_weather <- list_matrix_weather[[s]]
+  k <- 2
+  leg[k] <- "+ Water"
+  matrix_weather <- list_matrix_weather[[s]]
+  matrix_weather[,6] <- list_matrix_weather[[3]][,6] # use Lincoln rainfall/irrigation 
+  list_output[[k]] <- run_model(y = matrix(0, NDAYS, NOUT))
+  matrix_weather <- list_matrix_weather[[s]]
   
-  leg[2] <- "Destock-5%"
+  k <- 3
+  leg[k] <- "+ Grazing"
+  days_harvest <- list_days_harvest[[3]] # use Lincoln grazing pressure
+  list_output[[k]] <- run_model(y = matrix(0, NDAYS, NOUT))
   days_harvest <- list_days_harvest[[s]]
-  ii <- seq(length(days_harvest)/3*2+1, length(days_harvest))
-  days_harvest[ii] <- as.integer(days_harvest[ii] * 0.95) # destock
-  list_output[[2]] <- run_model(y = matrix(0, NDAYS, NOUT))
-  days_harvest <- list_days_harvest[[s]]
-  
-  leg[3] <- "Destock-10%"
-  days_harvest <- list_days_harvest[[s]]
-  ii <- seq(length(days_harvest)/3*2+1, length(days_harvest))
-  days_harvest[ii] <- as.integer(days_harvest[ii] * 0.9) # destock
-  list_output[[3]] <- run_model(y = matrix(0, NDAYS, NOUT))
-  days_harvest <- list_days_harvest[[s]]
-  
-  leg[4] <- "Destock-20%"
-  days_harvest <- list_days_harvest[[s]]
-  ii <- seq(length(days_harvest)/3*2+1, length(days_harvest))
-  days_harvest[ii] <- as.integer(days_harvest[ii] * 0.8) # destock
-  list_output[[4]] <- run_model(y = matrix(0, NDAYS, NOUT))
-  days_harvest <- list_days_harvest[[s]]
-  
-  leg[5] <- "Destock-30%"
-  days_harvest <- list_days_harvest[[s]]
-  ii <- seq(length(days_harvest)/3*2+1, length(days_harvest))
-  days_harvest[ii] <- as.integer(days_harvest[ii] * 0.7) # destock
-  list_output[[5]] <- run_model(y = matrix(0, NDAYS, NOUT))
-  days_harvest <- list_days_harvest[[s]]
-  
+
+  k <- 4
+  leg[k] <- "+ Temp"
+  matrix_weather <- list_matrix_weather[[s]]
+  matrix_weather[,4] <- list_matrix_weather[[3]][,4] # use Lincoln temperature
+  matrix_weather[,5] <- list_matrix_weather[[3]][,5] # use Lincoln temperature 
+  list_output[[k]] <- run_model(y = matrix(0, NDAYS, NOUT))
+  matrix_weather <- list_matrix_weather[[s]]
+
   # export output using functions in initialise_BASGRA_general.R
   file_table  = paste(scenario, "/basgra_trace_table_", s, ".txt", sep="")
   file_plot   = paste(scenario, "/basgra_trace_plots_", s, ".png", sep="")
